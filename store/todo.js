c8f66f2188f8d725e3990c1841578e6dbe6b6f6d
kwangeun ahn <ahnkwang7379@gmail.com>
2024-11-09 16:49:56 +0900

    [feat] store로 만드는 페이지를 위한 todo store 추가

--- store/todo.js
+++ store/todo.js
@@ -1,6 +1,6 @@
 /**
  * @description
- * 투두 리스트 스토어 버전을 위한 간단 스토어
+ * 투두 리스트 스토어 버전을 위한 스토어로 전통적인 Nuxt2의 스토어를 따라갑니다.
  * state: 상태값
  * getters: state를 그대로 내리거나 매핑해서 내리고싶을때
  * actions: api 호출
@@ -15,7 +15,6 @@
  */
 const initState = () => {
   return {
-    stateTest: 'isTest',
     todoList: [],
   };
 };
@@ -23,20 +22,22 @@ const initState = () => {
 export const state = () => initState();
 
 export const getters = {
-  stateTest: ({ stateTest }) => stateTest,
   todoList: ({ todoList }) => todoList,
 };
 
 export const actions = {
-  async getTodoList() {
+  /**
+   * @description
+   * todoList를 가져와 store에 세팅해줍니다.
+   */
+  async getTodoList({ commit }) {
     try {
       const res = await this.$axios.$get(`/rest/v1/todo`);
 
-      return res;
+      // mutation으로 state값 변경
+      commit('SET_TODO_LIST', res);
     } catch (e) {
       console.error(e);
-      // error 처리는 간단하게 빈 배열 내려서 ui에서 안그리는 방식.
-      return [];
     }
   },
 };
@@ -45,5 +46,9 @@ export const mutations = {
   // store 초기화
   RESET_TODO_STORE(state) {
     Object.assign(state, initState());
+  },
+  // set todoList
+  SET_TODO_LIST(state, todoList) {
+    state.todoList = [...state.todoList, ...todoList];
   },
 };
