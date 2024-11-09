c8e35e51be86c1c2f6cf37eb11962848c178d4a5
kwangeun ahn <ahnkwang7379@gmail.com>
2024-11-09 16:50:13 +0900

    [feat] store, prop, provide 버전 페이지 각각 추가

---
+++ pages/prop/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <article class="prop">
+    TODO: Page에서 api 호출한 후 아래로 내리는 버전
+  </article>
+</template>
+
+<script>
+/**
+ * @description
+ * prop 드릴링으로 만드는 TODO페이지
+ */
+
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'PropPage',
+  data() {
+    return {
+      todoList: [],
+    };
+  },
+  fetchOnServer: false,
+  async fetch() {
+    // TODO: fetch state값과 pagination 넣기
+    try {
+      const res = await this.getTodoList();
+      this.todoList = res;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  head() {
+    return {
+      title: 'Prop',
+    };
+  },
+  methods: {
+    ...mapActions(['getTodoList']),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.prop {
+  display: flex;
+}
+</style>
---
+++ pages/provide/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <article class="provide">
+    TODO: provide버전으로 page에서 api호출 후 하위 컴포넌트에서 inject로 꺼내
+    쓰는 방식으로 구현할 곳
+  </article>
+</template>
+
+<script>
+/**
+ * @description
+ * Provide & Inject로 만드는 TODO페이지
+ */
+
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'ProvidePage',
+  // provide 너무 커질 것 같으면 mixin으로 뺄것
+  provide() {
+    return {
+      todoList: () => this.todoList,
+    };
+  },
+  data() {
+    return {
+      todoList: [],
+    };
+  },
+  fetchOnServer: false,
+  async fetch() {
+    // TODO: fetch state값과 pagination 넣기
+    try {
+      const res = await this.getTodoList();
+      this.todoList = res;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  head() {
+    return {
+      title: 'Provide',
+    };
+  },
+  methods: {
+    ...mapActions(['getTodoList']),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.provide {
+  display: flex;
+}
+</style>
