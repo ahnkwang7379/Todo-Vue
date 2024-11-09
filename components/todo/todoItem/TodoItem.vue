1421bc42cdfc03577a831c09a50b61e16cb35ea6
kwangeun ahn <ahnkwang7379@gmail.com>
2024-11-09 16:50:32 +0900

    [feat] TodoList와 TodoItem 공용 컴포넌트 틀 잡는중

---
+++ components/todo/todoItem/TodoItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <nuxt-link :to="toLink" class="todo-item">
+    <div>내부 데이터 보여줄곳~</div>
+  </nuxt-link>
+</template>
+
+<script>
+/**
+ * @description
+ * TodoItem 각각의 컴포넌트
+ */
+export default {
+  name: 'TodoItem',
+  components: {},
+  props: {
+    id: { type: Number, required: true, default: -1 },
+  },
+  data: ({ $route, id }) => {
+    return {
+      // todoItem 링크
+      toLink: { name: `${$route.name}-id`, params: { id } },
+    };
+  },
+  computed: {
+    //
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.todo-item {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ components/todo/todoList/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="todo-list">
+    <li v-for="todo of todoList" :key="`todo-${todo.id}`">
+      <todo-item v-bind="todo" />
+    </li>
+  </ul>
+</template>
+
+<script>
+/**
+ * @description
+ * TodoList 컴포넌트
+ */
+
+import TodoItem from '../todoItem/TodoItem.vue';
+
+export default {
+  name: 'TodoList',
+  components: { TodoItem },
+  props: {
+    todoList: { type: Array, required: true, default: () => [] },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.todo-list {
+  display: flex;
+  flex-direction: column;
+}
+</style>
