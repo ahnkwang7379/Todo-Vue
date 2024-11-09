c6ed4006a1cd9cf75bd7ba032acea048a1f9cb22
kwangeun ahn <ahnkwang7379@gmail.com>
2024-11-09 16:50:13 +0900

    [feat] store, prop, provide 버전 페이지 각각 추가

---
+++ pages/store/_id.vue
@@ -0,0 +1,29 @@
+<template>
+  <article class="todo">
+    <div>TODO 아이템의 상세 페이지</div>
+  </article>
+</template>
+
+<script>
+/**
+ * @description
+ * 스토어로 만드는 TODO페이지
+ * TODO 상세
+ */
+export default {
+  name: 'TodoId',
+  // TODO: 상세페이지에서 어떤 작업이 가능할지 고민해볼것.
+  head() {
+    return {
+      title: '상세 정보',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.todo {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ pages/store/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <article class="store"></article>
+</template>
+
+<script>
+/**
+ * @description
+ * 스토어로 만드는 TODO페이지
+ */
+
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'StorePage',
+  fetchOnServer: false,
+  async fetch() {
+    try {
+      const res = await this.getTodoList();
+
+      console.log(res);
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  head() {
+    return {
+      title: 'Store',
+    };
+  },
+  methods: {
+    ...mapActions('todo', ['getTodoList']),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.store {
+  display: flex;
+  flex-direction: column;
+}
+</style>
