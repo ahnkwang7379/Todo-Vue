1e005b10af46d0238e09cd2d3030c3964e402c77
kwangeun ahn <ahnkwang7379@gmail.com>
2024-11-09 16:49:43 +0900

    [feat] 공용 스토어 추가

--- store/index.js
+++ store/index.js
@@ -1,24 +1,24 @@
 /**
  * @description
  * 전역에서 쓰이는 값 관리를 위한 스토어
+ * 현재는 todo에 쓰이는 api 호출부로 사용중으로 actions만 씁니다.
  */
 
-const initState = () => {
-  return {
-    stateTest: 'isTest',
-  };
-};
-
-export const state = () => initState();
-
-export const getters = {
-  stateTest: ({ stateTest }) => stateTest,
-};
-
 export const actions = {
-  // actions
-};
+  /**
+   * @description
+   * TodoList를 받아와 리턴
+   */
+  async getTodoList() {
+    try {
+      const res = await this.$axios.$get(`/rest/v1/todo`);
 
-export const mutations = {
-  // mutations
+      return res;
+    } catch (e) {
+      console.error(e);
+      // error 처리는 간단하게 빈 배열 내려서 ui에서 안그리는 방식.
+      return [];
+    }
+  },
+  // TODO: CRUD API
 };
