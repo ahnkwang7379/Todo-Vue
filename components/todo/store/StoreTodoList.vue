b65a95f11d75f64d82101d863de2bbe60b8440b9
kwangeun ahn <ahnkwang7379@gmail.com>
2024-11-10 22:04:47 +0900

    [feat] Todo Store페이지에서 쓰는 서비스 로직이 들어가는 wrapper 컴포넌트 추가

---
+++ components/todo/store/StoreTodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <todo-list :todo-list="todoList" />
+</template>
+
+<script>
+/**
+ * @description
+ * store페이지에서 쓰이는 TodoList를 래핑
+ */
+
+import { mapGetters } from 'vuex';
+
+import TodoList from '@/components/todo/common/todoList/TodoList.vue';
+
+export default {
+  name: 'StoreTodoList',
+  components: { TodoList },
+  computed: {
+    ...mapGetters('todo', ['todoList']),
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <article>
-    <test-component />
+    <h2>Vue2 & Nuxt2로 만드는 간단한 Todo입니다</h2>
+    <p>
+      Store방식, Props방식, Provide방식 총 3가지 방법으로 각각 구현했습니다.
+    </p>
   </article>
 </template>
 
@@ -10,10 +13,7 @@
  * 홈 화면
  */
 
-import TestComponent from '@/components/TestComponent.vue';
-
 export default {
   name: 'Index',
-  components: { TestComponent },
 };
 </script>
